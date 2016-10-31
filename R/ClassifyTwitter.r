de7a498976632f8f23e499d699f1508b517fd6e1
Chetna Warade <warade@gmail.com>
2016-10-31 16:24:19 -0400

    Personality Insights
    Added call to Watson Personality Insights to the classification of
    twitter user profile. Plot the JSON response received back from PI.

--- R/ClassifyTwitter.r
+++ R/ClassifyTwitter.r
@@ -72,6 +72,186 @@ return(data)
 }
 
 
+pi_url="https://gateway.watsonplatform.net/personality-insights/api/v2/profile"
+pi_username = "XXXX" # your own here 
+pi_password = "XXXX"  # your own here 
+pi_username_password = paste(pi_username,":",pi_password,sep="")
+
+personality_insights <- function(textTweets){
+    
+    response <- POST(url=pi_url,authenticate(pi_username,pi_password),add_headers("Content-Type"="text/plain","charset"="utf-8" ),body = textTweets)
+
+    data <- content(response)
+    
+return(data)    
+}
+
+#Unfold JSON response of Personality Insights
+
+plot_pi <- function(pidata){
+
+big5 <- data.frame()
+needs <- data.frame()
+values <- data.frame()
+openness <- data.frame()
+conscientiousness <- data.frame()
+extraversion <- data.frame()
+agreeableness <- data.frame()
+emotional_range <- data.frame()
+
+i = 10
+
+while(i < 184){
+
+	pval <- pidata[1,(i+2)] * 100	
+	#print(pval)	
+	
+	
+		
+		if((i == 10) || (i == 45) || (i == 80) || (i == 115) || (i == 150)) { #Big5
+			
+		temp <- data.frame(pval)
+		colnames(temp) <- pidata[1,i]
+		
+		if(length(big5) == 0)
+		big5 <- temp
+		else
+		big5 <- cbind(big5, temp)
+		}
+		
+	
+	
+	if((i > 10) && (i < 45)){ #Openness
+		
+		temp <- data.frame(pval)
+		colnames(temp) <- pidata[1,i]
+		
+		if(length(openness) == 0)
+		openness <- temp
+		else
+		openness <- cbind(openness, temp)
+	
+		}
+	
+	if((i > 45) && (i < 80)){ #Conscientiousness
+		
+		temp <- data.frame(pval)
+		colnames(temp) <- pidata[1,i]
+		
+		if(length(conscientiousness) == 0)
+		conscientiousness <- temp
+		else
+		conscientiousness <- cbind(conscientiousness, temp)
+	
+		}
+
+	if((i > 80) && (i < 115)){ #Extraversion
+		
+		temp <- data.frame(pval)
+		colnames(temp) <- pidata[1,i]
+		
+		if(length(extraversion) == 0)
+		extraversion <- temp
+		else
+		extraversion <- cbind(extraversion, temp)
+	
+		}
+	
+
+	if((i > 115) && (i < 150)){ #Agreeableness
+		
+		temp <- data.frame(pval)
+		colnames(temp) <- pidata[1,i]
+		
+		if(length(agreeableness) == 0)
+		agreeableness <- temp
+		else
+		agreeableness <- cbind(agreeableness, temp)
+	
+		}
+	
+
+	if((i > 150) && (i < 185)){ #Emotional Range
+		
+		temp <- data.frame(pval)
+		colnames(temp) <- pidata[1,i]
+		
+		if(length(emotional_range) == 0)
+		emotional_range <- temp
+		else
+		emotional_range <- cbind(emotional_range, temp)
+	
+		}
+	
+
+	
+	
+i <- i + 5	
+}		
+i = 191
+while(i < 251){ #Needs
+	pval <- pidata[1,(i+2)] * 100	
+	#print("needs")
+	#print(pval)	
+	
+		temp <- data.frame(pval)
+		colnames(temp) <- pidata[1,i]
+		
+		if(length(needs) == 0)
+		needs <- temp
+		else
+		needs <- cbind(needs, temp)
+		
+
+i <- i + 5	
+}	
+
+i = 257
+while(i < 280){ #Values
+	pval <- pidata[1,(i+2)] * 100	
+	#print("values")
+	#print(pval)	
+	
+		temp <- data.frame(pval)
+		colnames(temp) <- pidata[1,i]
+		
+		if(length(values) == 0)
+		values <- temp
+		else
+		values <- cbind(values, temp)
+		
+
+i <- i + 5	
+}	
+
+par(las = 2, mfrow = c(3,3)) #print x labels perpendicular to the x-axis
+
+bp <- barplot(unlist(big5),cex.axis=0.5,cex.names=0.5,main = "Big 5 Personality", ylab = "Score in Percentage%", ylim = c(0,100),col='deepskyblue3')
+text(bp,0,round(unlist(big5),1), pos = 3)
+
+bp <- barplot(unlist(needs),cex.axis=0.5,cex.names=0.5,main = "Needs", ylab = "Score in Percentage%", ylim = c(0,100),col='seagreen3')
+text(bp,0,round(unlist(needs),1), pos = 3)
+
+bp <- barplot(unlist(values),cex.axis=0.5,cex.names=0.5,main = "Values", ylab = "Score in Percentage%", ylim = c(0,100),col='orangered3')
+text(bp,0,round(unlist(values),1), pos = 3)
+
+bp <- barplot(unlist(openness),cex.axis=0.5,cex.names=0.5,main = "Openness", ylab = "Score in Percentage%", ylim = c(0,100),col='deepskyblue')
+text(bp,0,round(unlist(openness),1), pos = 3)
+
+bp <- barplot(unlist(conscientiousness),cex.axis=0.5,cex.names=0.5,main = "Conscientiousness", ylab = "Score in Percentage%", ylim = c(0,100),col='deepskyblue')
+text(bp,0,round(unlist(conscientiousness),1), pos = 3)
+
+bp <- barplot(unlist(extraversion),cex.axis=0.5,cex.names=0.5,main = "Extraversion", ylab = "Score in Percentage%", ylim = c(0,100),col='deepskyblue')
+text(bp,0,round(unlist(extraversion),1), pos = 3)
+
+bp <- barplot(unlist(agreeableness),cex.axis=0.5,cex.names=0.5,main = "Agreeableness", ylab = "Score in Percentage%", ylim = c(0,100),col='deepskyblue')
+text(bp,0,round(unlist(agreeableness),1), pos = 3)
+
+bp <- barplot(unlist(emotional_range),cex.axis=0.5,cex.names=0.5,main = "Emotional Range", ylab = "Score in Percentage%", ylim = c(0,100),col='deepskyblue')
+text(bp,0,round(unlist(emotional_range),1), pos = 3)
+
+}
+
 compute_score <- function(data){
 	tonedata <- as.data.frame(data$document_tone$tone_categories)
 
@@ -213,12 +393,12 @@ stddev <- function(dfclassify){
 classify <- data.frame()
 summarytw <- data.frame()
 
-for(j in 1:10){
+for(j in 1:2){
 
 searchstring <- str_replace(paste("from:",authors[j,1]), ' ','')
 
 #print(searchstring)
-tweets <- searchTwitter(searchstring, n=2)
+tweets <- searchTwitter(searchstring, n=100)
 typeof(tweets)
 length(tweets)
 
@@ -263,7 +443,14 @@ for(i in 1:length(tweets)){
    colnames(classifytw) <- c('Author','Tweet','Positive','Negative','Neutral', colnames(df_scorelist))   
    classify <- rbind(classify,classifytw)
    summarytw <- rbind(summarytw,stddev(classifytw))
- }   
+ 
+
+    #Call Personality Insights for each Twitter author
+                    
+    personality_data <- personality_insights(textTweets)
+    plot_pi(as.data.frame(personality_data["tree"]))   
+}
+                   
 }    
 
 classify.table <- xtable(classify)
@@ -272,6 +459,8 @@ print.xtable(classify.table, type="html", file = "")
 print("Summary")                    
 summarytw.table <- xtable(summarytw)
 print.xtable(summarytw.table, type="html", file = "")
+
+perosonality_data
 
 print("<p>Summary</p>",type="html",file = "")
 
